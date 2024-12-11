3f1a2f010304f8c15cbedc5a486190319c4ad052
Aysla <ayslaloureiro@gmail.com>
2024-12-11 13:04:36 +0000

    cria arquivo card

--- src/Card.js
+++ src/Card.js
@@ -1,36 +1,49 @@
-export default function Card({ title, description, skills, projects }) {
+import close from "./close-icon.png";
+export default function Card({
+  title,
+  description,
+  skills,
+  projects,
+  onClose,
+}) {
   return (
-    <div className="cards-container">
-      <h1 className="card-title">{title}</h1>
-      {description && <p className="card-paragraph">{description}</p>}
-      {skills && (
-        <ul>
-          {skills.map((skill) => {
-            return <li>{skill}</li>;
-          })}
-        </ul>
-      )}
-      {projects && (
-        <ul>
-          {projects.map((projects) => {
-            return (
-              <li>
-                <a
-                  href={projects.url}
-                  target="_blank"
-                  style={{
-                    TextDecoderation: "none",
-                    color: "#923333",
-                    fontSize: "20px",
-                  }}
-                >
-                  {projects.name}{" "}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </div>
+    <>
+      {/* <div className="overlay"></div> */}
+      <div className="cards-container">
+        <button className="close-icon" onClick={onClose}>
+          <img src={close}></img>
+        </button>
+        <h1 className="card-title">{title}</h1>
+        {description && <p className="card-paragraph">{description}</p>}
+        {skills && (
+          <ul>
+            {skills.map((skill) => {
+              return <li>{skill}</li>;
+            })}
+          </ul>
+        )}
+        {projects && (
+          <ul>
+            {projects.map((projects) => {
+              return (
+                <li>
+                  <a
+                    href={projects.url}
+                    target="_blank"
+                    style={{
+                      TextDecoderation: "none",
+                      color: "#923333",
+                      fontSize: "20px",
+                    }}
+                  >
+                    {projects.name}{" "}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    </>
   );
 }
